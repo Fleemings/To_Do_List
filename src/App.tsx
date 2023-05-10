47b771a768fbc81d8ca320c021ed7c64f87140bd
Milena Fleming <milenasapucaia29@gmail.com>
2023-05-10 17:21:27 +0100

    Implemented logic of the form task and form list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import AppStyle from './App.module.css'
+import AppStyle from './App.module.css';
+import FormTask from './Components/Form/FormTask';
+import FormList from './Components/Form/FormList';
+
+// Interface
+import { ITask } from './Interfaces/ITask';
+
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id;
+      })
+    );
+  };
   return (
-    <div className="App">
-      <Header/>
-      <main className={AppStyle.main}></main>
-      <Footer/>
+    <div className={AppStyle.main}>
+      <Header />
+      <FormTask
+        btnText='Create Task'
+        taskList={taskList}
+        setTaskList={setTaskList}
+      />
+      <FormList taskList={taskList} handleDelete={deleteTask} />
+      <Footer />
     </div>
   );
 }
